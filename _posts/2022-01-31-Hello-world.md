76944e55f9a9af20e31082a1a5d7a4ac036ecb3d
pietro-sillano <pietrosillano@gmail.com>
2022-01-31 17:41:51 +0100

    added latex js script to single.html

---
+++ _posts/2022-01-31-Hello-world.md
@@ -0,0 +1,9 @@
+---
+title: "Hello World"
+#categories:
+#tags:
+#cover-img: ../assets/img/snake.png
+
+---
+
+It is done! Welcome to my new website.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Hello World",
+        "excerpt":"It is done! Welcome to my new website.  ","categories": [],
+        "tags": [],
+        "url": "/Hello-world/",
+        "teaser": null
+      },{
         "title": "EEG Analysis",
         "excerpt":"This project has been developed during the 2021 hackathon organized by G-Tec. In these two days, we analyzed a motor imagery Brain Computer Interfaces (BCIs) dataset from chronic stroke patients. In recent years, BCIs have been widely used as rehabilitation tools to help the recovery process.. Thanks to BCIs, we...","categories": ["project"],
         "tags": [],
@@ -15,5 +21,11 @@ var store = [{
         "excerpt":" ","categories": ["project"],
         "tags": ["Reinforcement Learning"],
         "url": "/projects/2021-08-20-SnakeAI/",
+        "teaser": null
+      },{
+        "title": "Particles in Electromagnetic Fields",
+        "excerpt":"Introduzione In numerosi campi della fisica é importante studiare la dinamica delle singole particelle, sono peró pochi i casi in cui una soluzione analitica é possibile. Per esempio, se considero una particella carica in movimento interagisce con il campo elettromagnetico, l’equazione che ne descrive la dinamica é la seguente :...","categories": ["project"],
+        "tags": ["Physics Simulation"],
+        "url": "/projects/2022-01-31-EMfield/",
         "teaser": null
       }]
