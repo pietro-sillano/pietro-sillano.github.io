3d0a179eec852419355f191c35ba4724a5ae69fd
pietro-sillano <pietrosillano@gmail.com>
2022-02-23 16:24:19 +0100

    upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "/Hello-world/",
         "teaser": null
       },{
-        "title": "Particles in Electromagnetic Fields",
+        "title": "Particles Simulation in EM Fields",
         "excerpt":"In many fields of physics, it is important to study the dynamics of single particles, but there are few cases in which an analytical solution is possible. For example, if I consider a charged particle in motion interacting with the electromagnetic field, the equation that describes its dynamics is the...","categories": ["project"],
         "tags": ["Physics Simulation"],
         "url": "/projects/1.EMfield/",
@@ -15,5 +15,11 @@ var store = [{
         "excerpt":"This project has been developed during the 2021 hackathon organized by G-Tec. In these two days, we analyzed a motor imagery Brain Computer Interfaces (BCIs) dataset from chronic stroke patients. In recent years, BCIs have been widely used as rehabilitation tools to help the recovery process.. Thanks to BCIs, we...","categories": ["project"],
         "tags": [],
         "url": "/projects/2.EEG_Analysis/",
+        "teaser": null
+      },{
+        "title": "Autoencoder + SINDy + Pendulum",
+        "excerpt":"Introduction The use of neural networks or deep learning for the prediction of dynamics of dynamical systems presents some criticalities, among which problems of generalization outside the training set examples and also a lack of interpretability of the model obtained, due to the great the number of parameters of such...","categories": ["project"],
+        "tags": ["Autoencoders"],
+        "url": "/projects/3.SINDY/",
         "teaser": null
       }]
